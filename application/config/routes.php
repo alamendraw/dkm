3557e5a2752d51a1604069197875379db4848b1b
Alamul huda <alamulhuda.tsi@gmail.com>
2020-08-27 11:52:48 +0700

    First Commit

---
+++ application/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+$route['default_controller'] = 'login';
+$route['404_override'] = 'home/get_404';
+$route['coming_soon'] = 'home/coming_soon';
+$route['dkm'] = 'login';
+$route['translate_uri_dashes'] = FALSE;
+
+$route['laporan-jumat'] = 'laporan/keuangan/jumat';
