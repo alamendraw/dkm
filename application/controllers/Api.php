68e4eb7ac8bf8b4725c2c34ec3aa953d400ae0f6
Alamul huda <alamulhuda.tsi@gmail.com>
2021-02-14 12:26:38 +0700

    Add Api

---
+++ application/controllers/Api.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct(); 
+		 
+	}
+	public function get_data() { 		
+		header('Content-Type: application/json');	
+		$sql = $this->db->query("SELECT date tanggal, debet nilai, description keterangan from transaction where debet !=0 limit 10");
+		$data = $sql->result_array();
+		echo json_encode($data);
+	} 
+}
