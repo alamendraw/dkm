5cfedf9da12445102c432582f4ef03b5d5b03083
Alamul huda <alamulhuda.tsi@gmail.com>
2021-02-14 13:13:40 +0700

    update api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -8,6 +8,7 @@ class Api extends CI_Controller {
 		 
 	}
 	public function get_data() { 		
+		header('Access-Control-Allow-Origin: *'); 
 		header('Content-Type: application/json');	
 		$sql = $this->db->query("SELECT date tanggal, debet nilai, description keterangan from transaction where debet !=0 limit 10");
 		$data = $sql->result_array();
