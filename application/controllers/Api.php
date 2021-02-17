467017c2f32828eac7332bf7f34813130217ccae
Alamul huda <alamulhuda.tsi@gmail.com>
2021-02-17 14:35:52 +0700

    Update Api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,14 +4,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Api extends CI_Controller {
 	public function __construct()
 	{
-		parent::__construct(); 
-		 
+		parent::__construct(); 		 
+
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+		header('Content-Type: application/json');
+
+		$this->load->model('keuangan/pendapatans'); 
 	}
-	public function get_data() { 		
-		header('Access-Control-Allow-Origin: *'); 
-		header('Content-Type: application/json');	
-		$sql = $this->db->query("SELECT date tanggal, debet nilai, description keterangan from transaction where debet !=0 limit 10");
+
+	private function get_kas(){
+		$last_no = $this->pendapatans->get_last_kas();
+		if($last_no){  
+			$data = sprintf("%05d", $last_no+1); 
+		}else{
+			$data = '00001';
+		}
+
+		return $data;
+	}
+
+	public function pemasukan_get() {
+		$item=[];
+		$sql = $this->db->query("SELECT id,date tanggal, debet nilai, description keterangan from transaction where debet !=0 order by no_kas desc");
 		$data = $sql->result_array();
-		echo json_encode($data);
-	} 
+		foreach($data as $row){
+			array_push($item,array(
+				'id' => date_indo($row['id']),
+				'tanggal' => date_indo($row['tanggal']),
+				'nilai' => number_format($row['nilai']),
+				'keterangan' => $row['keterangan']
+
+			));
+		}
+		
+		echo json_encode($item);
+	}
+
+	public function pemasukan_post(){
+		$request = json_decode(file_get_contents('php://input'));
+		$field['no_kas'] = $this->get_kas();
+		$field['date'] = $request->tanggal;
+		$field['debet'] = $request->nilai;
+		$field['description'] = $request->keterangan;
+		$save = $this->pendapatans->insert($field); 
+		$result['message'] = 'Data Tersimpan';
+		
+		echo json_encode($result);
+	}
 }
